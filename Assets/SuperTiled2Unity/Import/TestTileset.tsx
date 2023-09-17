7e374bb232b2de605a0dcc5244d5856fed98e774
Jared <jaredcallupelopez@gmail.com>
2023-09-17 16:32:09 -0700

    Super Unity2Tiled stuff + movement playtest scene

---
+++ Assets/SuperTiled2Unity/Import/TestTileset.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="TestTileset" tilewidth="16" tileheight="16" spacing="1" tilecount="400" columns="20">
+ <image source="monochrome_tilemap.png" trans="55ff00" width="339" height="339"/>
+</tileset>
