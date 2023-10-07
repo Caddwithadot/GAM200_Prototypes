3cc7b73de76b4cd9dcd046df08cf2c677056fa60
Jared <jaredcallupelopez@gmail.com>
2023-10-06 17:41:19 -0700

    Main menu and other map stuff

---
+++ Assets/SuperTiled2Unity/Import/Cable_Tileset.tsx
@@ -0,0 +1,64 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="Cable_Tileset" tilewidth="16" tileheight="16" tilecount="10" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="9">
+  <properties>
+   <property name="name" value="T_left"/>
+  </properties>
+  <image width="16" height="16" source="../../../../../../Downloads/T_left.png"/>
+ </tile>
+ <tile id="10">
+  <properties>
+   <property name="name" value="T_up"/>
+  </properties>
+  <image width="16" height="16" source="../../../../../../Downloads/T_up.png"/>
+ </tile>
+ <tile id="11">
+  <properties>
+   <property name="name" value="T_right"/>
+  </properties>
+  <image width="16" height="16" source="../../../../../../Downloads/T_right.png"/>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="name" value="T_down"/>
+  </properties>
+  <image width="16" height="16" source="../../../../../../Downloads/T_down.png"/>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="name" value="C_tr"/>
+  </properties>
+  <image width="16" height="16" source="../../../../../../Downloads/C_tr.png"/>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="name" value="C_br"/>
+  </properties>
+  <image width="16" height="16" source="../../../../../../Downloads/C_br.png"/>
+ </tile>
+ <tile id="15">
+  <properties>
+   <property name="name" value="C_bl"/>
+  </properties>
+  <image width="16" height="16" source="../../../../../../Downloads/C_bl.png"/>
+ </tile>
+ <tile id="16">
+  <properties>
+   <property name="name" value="C_tl"/>
+  </properties>
+  <image width="16" height="16" source="../../../../../../Downloads/C_tl.png"/>
+ </tile>
+ <tile id="17">
+  <properties>
+   <property name="name" value="H"/>
+  </properties>
+  <image width="16" height="16" source="../../../../../../Downloads/H.png"/>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="name" value="V"/>
+  </properties>
+  <image width="16" height="16" source="../../../../../../Downloads/V.png"/>
+ </tile>
+</tileset>
